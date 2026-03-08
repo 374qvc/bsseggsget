e1484f812073a06c3c7d345bfa56a91c119de553
374qvc <damoidz@gmail.com>
2026-03-08 18:51:59 +0300

    getgold.lua

---
+++ getgold.lua
@@ -0,0 +1,32 @@
+local player = game.Players.LocalPlayer
+local tweenService = game:GetService("TweenService")
+local character = player.Character
+if not character then
+    character = player.CharacterAdded:Wait()
+end
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart", 5)
+
+local function tweenToPosition(targetCFrame, duration)
+
+    if not humanoidRootPart then
+        warn("HumanoidRootPart не найден!")
+        return
+    end
+
+    local tweenInfo = TweenInfo.new(
+        duration or 2,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.Out,
+        0,
+        false,
+        0
+    )
+    local tween = tweenService:Create(humanoidRootPart, tweenInfo, {
+        CFrame = targetCFrame
+    })
+    tween:Play()
+end
+
+tweenToPosition(CFrame.new(-57.1720009, 150.0280781, 106.606125), 3)
+wait(3)
+tweenToPosition(CFrame.new(83.8066254, 69.4766388, -142.149338), 3)
